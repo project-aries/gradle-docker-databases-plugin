6af47d3a74a9514f94e48d7327512b79eaa3c4fc
dancc <Chris.Dancy@pega.com>
2018-06-16 18:33:26 -0400

    Add functional test for db2

---
+++ src/functionalTest/groovy/com/aries/gradle/docker/database/plugin/Db2FunctionalTest.groovy
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aries.gradle.docker.database.plugin
+
+import org.gradle.testkit.runner.BuildResult
+import spock.lang.Timeout
+
+import static java.util.concurrent.TimeUnit.MINUTES
+
+/**
+ *
+ *  Functional tests to perform up, stop, and down tasks on a dockerized database.
+ *
+ */
+class Db2FunctionalTest extends AbstractFunctionalTest {
+
+    def "Can start, stop, and remove a db2 application stack"() {
+
+        String uuid = randomString()
+        buildFile << """
+            
+            applications {
+                db2 {
+                    id = "${uuid}"
+                }
+            }
+            task up(dependsOn: ['db2Up'])
+            
+            task stop(dependsOn: ['db2Stop'])
+
+            task down(dependsOn: ['db2Down'])
+        """
+
+        when:
+            BuildResult result = build('up', 'stop', 'down')
+
+        then:
+            result.output.contains('is not running or available to inspect')
+            result.output.contains('Inspecting container with ID')
+            result.output.contains('Created container with ID')
+            result.output.contains('Starting liveness probe on container')
+            result.output.contains('Running exec-stop on container with ID')
+            result.output.contains('Removing container with ID')
+            result.output.contains('RestartContainer SKIPPED')
+            !result.output.contains('ListImages SKIPPED')
+    }
+}
